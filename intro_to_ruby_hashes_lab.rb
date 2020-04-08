dcf0831ef0e325998be53874fe7282d3f6cc39b3
JC Ramirez <jsmnramirez25@gmail.com>
2020-04-08 23:24:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -31,12 +31,17 @@ def monopoly_with_third_tier
     :railroads => {
       :pieces => 4,
       :rent_in_dollars => {
-        :one_pieces_owned => 25
-        :two_pieces_owned => 50
-        :three_pieces_owned => 100
+        :one_pieces_owned => 25,
+        :two_pieces_owned => 50,
+        :three_pieces_owned => 100,
         :four_pieces_owned => 200
       },
-      :names => {}
+      :names => {
+        :reading_railroad => {},
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
+        :shortlines_railroad => {}
+      }
     }
   }
 
